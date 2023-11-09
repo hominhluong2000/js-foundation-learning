fc37629400ddbcaaa729b0a5ec4df2417e842ace
LuongHo <hominhluong170420@gmail.com>
2023-11-09 23:24:34 +0700

    Practice lesson1

---
+++ lesson1.js
@@ -0,0 +1,102 @@
+// console.log("Hello World!"); {
+//     console.log("Hello World!"); {
+//         console.log("Hello World!"); {
+//             console.log("Hello World!");
+//         }
+//     }
+// }
+// console.log("Hello World!");
+
+// {
+//     let a = 3; {
+//         console.log(a);
+//     }
+// }
+
+// console.log(a);
+
+// let la bien cuc bo (bi gioi han boi cac block level)
+// let khong the ghi de`
+// ngta thuong dung let hoac const de khai bao variable. Han che su dung var
+// let a = null;
+// console.log(a);
+
+// let a = 2;
+
+// {
+//     var a = 7;
+// }
+// console.log(a);
+
+
+
+// var la global variale
+// var co the ghi de lai variable
+// var a = 7;
+// var a = 11;
+// console.log(a);
+
+// const de khai bao variable la hang so khong thay doi
+// const PI = 3.14;
+// PI = 3;
+
+// const objA = {};
+// console.log(objA);
+// objA["name"] = "Tho Test";
+// console.log(objA);
+
+// khong nen dung cach nay
+// khi khai bao nhu vay thi day nhu la 1 global variable
+// {
+//     x = 3;
+// }
+// console.log(x);
+
+// let a = 2;
+// let b = 4;
+// let c = a ** b; // ** la toan tu dung cho so mu~
+// console.log(c);
+
+// let a = 10;
+// let b = 10 % 3; // %3 la phep chia lay phan du
+// console.log(b);
+
+// let a = 1;
+// console.log(++a); // ++a la + truoc sau do moi in. a++ la in ra truoc sau do moi +
+// console.log(a);
+
+// let a = 3;
+// let b = 4;
+// a += b; // cach viet tat cua a = a + b (+,-,*,/,%,** deu su dung duoc)
+// console.log(a);
+
+// let a = 3;
+// let b = 3;
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+// console.log(a == b);
+// console.log(a = b); // khong duoc dung = cho toan tu
+
+// if (a = b) {
+//     console.log("hi");
+// }
+
+// let a = "Tho";
+// let b = " Test";
+// console.log(a + b);
+
+// a += b;
+// console.log(a);
+
+// console.log((5 === "hihi")); // === tuc la so sanh luon ca cung kieu du lieu
+
+// let a = 5;
+// console.log(a % 2 == 0 && a < 6 && a > 0); // && la tat ca dieu kien phai dung thi moi la true
+
+// console.log((a % 2 == 0 || a < 6 || a == 0)); // || chi can 1 trong so dieu kien dung thi la true
+
+// console.log((a % 2 == 0 && (a < 6 || a == 0)));
+
+// console.log(!(a > 6)); // toan tu phu dinh
