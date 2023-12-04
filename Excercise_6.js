9a4dfa5cf25e8e2990ddca016855fccf94c32412
LuongHo <hominhluong170420@gmail.com>
2023-12-04 17:03:51 +0700

    Practice ex5, ex6

---
+++ Excercise_6.js
@@ -0,0 +1,15 @@
+// Dùng callback, promise, async/await để viết chức năng của 1 chiếc đồng hồ, bao gồm chức năng đếm ngược và đếm xuôi. 
+// Đồng hồ sẽ nhận input là số N(số giây), mỗi một giây count down hoặc count up sẽ in ra thông báo tương ứng.
+
+function Clock(type, N){
+    switch (type){
+        case 'countUp':
+            return console.log("abc");
+        case 'countDown':
+            return console.log("def");
+        default:
+            return console.log("input incorrect");
+    }
+}
+
+Clock('countUp');
